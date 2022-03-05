5fff05395c0b44ee806b03fb2d1abe91c2160c10
lvcts <https://github.com/lvcts/projects.git>
2022-03-05 22:48:21 +0800

    ...

---
+++ day84.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+    <h1> Menghitung Luas PERSEGI</h1>
+    <?php
+    $a = 0;
+    $b = 0;
+    $panjang1 = 5; //in cm
+    $lebar1 = 7;
+    if ($panjang1 != $a || $lebar1 != $b) {
+        $rumus1 = $panjang1 * $lebar1;
+    }
+    echo "Jika panjang = $panjang1<br/>";
+    echo "Jika lebar = $lebar1<br/>";
+    echo "HASIL RUMUS  menyatakan bahwa Luas Persegi Panjang adalah Luas = P x L = $rumus1 cm <br/>";
+    ?>
+</body>
+
+</html>
