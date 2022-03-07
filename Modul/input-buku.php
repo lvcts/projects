468613686ee65c10d573ac127f96b0f2bd8aa5f9
lvcts <64696964+lvcts@users.noreply.github.com>
2022-03-07 23:15:03 +0800

    ubah lagi

--- Modul/input-buku.php
+++ Modul/input-buku.php
@@ -2,9 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <style>
@@ -42,7 +39,7 @@
         </select>
         <br><br>
 
-        <input type="submit" name="submit" value="Simpan">
+        <input type="submit" value="Simpan">
     </form>
 </body>
 
