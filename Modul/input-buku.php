d12d73ad34d494d1d9579db3fb694bb67a900479
lvcts <64696964+lvcts@users.noreply.github.com>
2022-03-13 23:53:52 +0800

    hhhhh

--- Modul/input-buku.php
+++ Modul/input-buku.php
@@ -1,46 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Document</title>
-</head>
-<style>
-
-</style>
-
-<body>
-    <form action="./database/proses-simpan.php" method="POST">
-        <p>
-            Masukkan data buku :
-        </p>
-        <label> ID Buku </label>
-        <input type="text" name="id_buku">
-        <br><br>
-        <label> Judul Buku </label>
-        <input type="text" name="judul">
-        <br><br>
-        <label> Nama Penulis </label>
-        <input type="text" name="nama_penulis">
-        <br><br>
-        <label> Nama Penerbit </label>
-        <input type="text" name="nama_penerbit">
-        <br><br>
-        <label> Tahun Terbit </label>
-        <input type="text" name="tahun_terbit">
-        <br><br>
-        <label> Rak </label>
-        <select name="rak">
-            <option value="A1">A1</option>
-            <option value="A2">A2</option>
-            <option value="B1">B1</option>
-            <option value="B2">B2</option>
-            <option value="C1">C1</option>
-            <option value="C2">C2</option>
-        </select>
-        <br><br>
-
-        <input type="submit" value="Simpan">
-    </form>
+<body class="bg-dark font-monospace">
+  <div class="container-fluid">
+    <!-- NAvbar-->
+    <nav class="navbar navbar-dark" id="navbar">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Siti Zahra Salma</span>
+      </div>
+    </nav>
+    <!--Jumbotron-->
+    <div class="jumbotron text-center " id="jumbotron">
+      <!--Images-->
+      <img src="https://scontent.fupg2-1.fna.fbcdn.net/v/t39.30808-6/234411552_1229058540853858_8649924542933551414_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHHuS6ufo26xpX7qk2j-se-T5wNPVNmpx9PnA09U2anH84WKYvUyAJRM9Cb8GCX2L7U7ISQuMTLQPftuNHVyyg3&_nc_ohc=qujnttBfBLEAX_xM3AR&_nc_zt=23&_nc_ht=scontent.fupg2-1.fna&oh=00_AT8h-Mpc9yvRudseHgpfOauRZz2y6uYzot5I8-WMoYewSw&oe=6232FED3" class="img-thumbnail rounded-circle" alt="..." width="280">
+      <h1>Siti Zahra Salma</h1>
+      <p class="lead" id="lead">"Hellowww"</p>
+    </div>
+    <!--Container-->
+    <div class="container" id="container">
+      <div class="row text-center" id="row">
+        <div class="col">
+          <h3>Hobby</h3>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/86/73/15/86731535bbd5bd14984d0a3f8ed57548.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby1">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/82/ca/ab/82caab8fab08a4f00684f35bf2d8d575.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby2">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/16/4e/af/164eaf7001292201868c52f7f39aa6b5.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby3">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/89/5b/75/895b7535d219f2760adbb7b3967da3d8.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby4">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/ff/bd/7e/ffbd7e12043dc352dfe5f2cca7887f97.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby5">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="coloum">
+            <img src="https://i.pinimg.com/564x/32/a2/61/32a261f897e0768f49fdd551a2a6dad2.jpg" class="bd-placeholder-img rounded-circle" alt="..." width="140" height="140">
+            <p class="text" id="hobby6">
+              <br>
+              ...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 </body>
-
-</html>
