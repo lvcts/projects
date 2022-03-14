6b334bf8b478dd3a635f9a79c0659e075686c083
lvcts <64696964+lvcts@users.noreply.github.com>
2022-03-14 10:11:28 +0800

    js

---
+++ Modul/script.js
@@ -0,0 +1,109 @@
+const jumbotron = document.getElementById("jumbotron");
+jumbotron.style.backgroundColor = "#5a5a5a";
+
+
+document.getElementById("hobby1").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  document.getElementById("hobby1").innerHTML =
+    "Hobbyku tentu saja belajar sesuatu yang baru dan menarik. Seperti belajar pemrograman tentunya.";
+}
+document.getElementById("hobby2").onclick = function () {
+  myFunction1();
+};
+
+function myFunction1() {
+  document.getElementById("hobby2").innerHTML =
+    "Hobbyku yang lain yaitu fotography. Dengan melakukan hobby tersebut dapat menyalurkan stress yang ada pada fikiranku.";
+}
+document.getElementById("hobby3").onclick = function () {
+  myFunction2();
+};
+
+function myFunction2() {
+  document.getElementById("hobby3").innerHTML =
+    "Aku merupakan seorang manusia biasa, tentu saja aku juga memiliki hobby menonton movie, anime, kdrma seperti wanita pada umumnya";
+}
+document.getElementById("hobby4").onclick = function () {
+  myFunction3();
+};
+
+function myFunction3() {
+  document.getElementById("hobby4").innerHTML =
+    "Selanjutnya, hobby yang sedikit merugikan namun dapat mempertahankan mental health adalah bermain game. Aku menyukai segala jenis game yang dapat bertahan di processor handphoneku";
+}
+document.getElementById("hobby5").onclick = function () {
+  myFunction4();
+};
+
+function myFunction4() {
+  document.getElementById("hobby5").innerHTML =
+    " Hobby yang paling kusukai setelah fotography adalah traveling. Meskipun kesempatan untuk mengimplementasikan hobby ini sangat sedikit tapi tetap saja sekali bercengkrama dengan alam rasanya luar biasa.";
+}
+document.getElementById("hobby6").onclick = function () {
+  myFunction5();
+};
+
+function myFunction5() {
+  document.getElementById("hobby6").innerHTML =
+    " Terakhir hobby yang tidak jelas asal usulnya adalah aku hobby memikirkan sesuatu yang rumit. Dalam suatu kasus fikiranku kadang memperdebatkan antara bumi bulat atau datar.";
+}
+
+const row = document.getElementById("row")
+row.style.visibility = "hidden";
+if (row.onclick) {
+    row.style.visibility = "visibleconst jumbotron = document.getElementById("jumbotron");
+jumbotron.style.backgroundColor = "#5a5a5a";
+
+document.getElementById("hobby1").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  document.getElementById("hobby1").innerHTML =
+    "Hobbyku tentu saja belajar sesuatu yang baru dan menarik. Seperti belajar pemrograman tentunya.";
+}
+document.getElementById("hobby2").onclick = function () {
+  myFunction1();
+};
+
+function myFunction1() {
+  document.getElementById("hobby2").innerHTML =
+    "Hobbyku yang lain yaitu fotography. Dengan melakukan hobby tersebut dapat menyalurkan stress yang ada pada fikiranku.";
+}
+document.getElementById("hobby3").onclick = function () {
+  myFunction2();
+};
+
+function myFunction2() {
+  document.getElementById("hobby3").innerHTML =
+    "Aku merupakan seorang manusia biasa, tentu saja aku juga memiliki hobby menonton movie, anime, kdrma seperti wanita pada umumnya";
+}
+document.getElementById("hobby4").onclick = function () {
+  myFunction3();
+};
+
+function myFunction3() {
+  document.getElementById("hobby4").innerHTML =
+    "Selanjutnya, hobby yang sedikit merugikan namun dapat mempertahankan mental health adalah bermain game. Aku menyukai segala jenis game yang dapat bertahan di processor handphoneku";
+}
+document.getElementById("hobby5").onclick = function () {
+  myFunction4();
+};
+
+function myFunction4() {
+  document.getElementById("hobby5").innerHTML =
+    " Hobby yang paling kusukai setelah fotography adalah traveling. Meskipun kesempatan untuk mengimplementasikan hobby ini sangat sedikit tapi tetap saja sekali bercengkrama dengan alam rasanya luar biasa.";
+}
+document.getElementById("hobby6").onclick = function () {
+  myFunction5();
+};
+
+function myFunction5() {
+  document.getElementById("hobby6").innerHTML =
+    " Terakhir hobby yang tidak jelas asal usulnya adalah aku hobby memikirkan sesuatu yang rumit. Dalam suatu kasus fikiranku kadang memperdebatkan antara bumi bulat atau datar.";
+}
+";
+}
